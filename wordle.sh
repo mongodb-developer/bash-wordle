045bffd6c368ff9f859141960c36b423666f820d
Charles Santos <thyarles@gmail.com>
2022-02-24 18:26:20 -0300

    ADD Brazilian words

--- wordle.sh
+++ wordle.sh
@@ -32,13 +32,13 @@ fi
 echo -e "Ok, I picked a word, try to guess\n"
 GO_ON=1
 TRIES=0
-while [ $GO_ON -eq 1 ]; do
+while [[ $GO_ON -eq 1 ]]; do
   TRIES=$(expr $TRIES + 1)
   read -n 5 -p "What is your guess: " USER_GUESS
   USER_GUESS=$(echo "$USER_GUESS" | awk '{print toupper($0)}')
   STATE=""
   for i in {0..4}; do
-    if [ "${WORD:i:1}" == "${USER_GUESS:i:1}" ]; then
+    if [[ "${WORD:i:1}" == "${USER_GUESS:i:1}" ]]; then
       STATE=$STATE"🟩"
     elif [[ $WORD =~ "${USER_GUESS:i:1}" ]]; then
       STATE=$STATE"🟨"
@@ -47,10 +47,10 @@ while [ $GO_ON -eq 1 ]; do
     fi
   done
   echo "  "$STATE
-  if [ $USER_GUESS == $WORD ]; then
+  if [[ $USER_GUESS == $WORD ]]; then
     echo -e "You won!"
     GO_ON=0
-  elif [ $TRIES == 5 ]; then
+  elif [[ $TRIES == 5 ]]; then
     echo -e "You failed.\nThe word was "$WORD
     GO_ON=0
   fi
